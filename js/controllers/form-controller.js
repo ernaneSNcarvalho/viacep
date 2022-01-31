f63569517c1a4cbdcd25fd18e319b93acb0ba7dd
ernane <ernanecarvalho89@gmail.com>
2022-01-31 14:43:15 -0300

    Projeto viacep inicializado

---
+++ js/controllers/form-controller.js
@@ -0,0 +1,108 @@
+import Address from '../models/address.js';
+import * as addressService from '../services/address-services.js'
+import * as listController from './list-controller.js';
+
+function State() {
+    this.address = new Address();
+    this.btnSave = null;
+    this.btnClear = null;
+
+    this.inputCep = null;
+    this.inputStreet = null;
+    this.inputNumber = null;
+    this.inputCity = null;
+
+    this.errorCep = null;
+    this.errorNumber = null;
+}
+
+const state = new State();
+
+export function init() {
+    state.inputCep = document.forms.newAddress.cep;
+    state.inputStreet = document.forms.newAddress.street;
+    state.inputNumber = document.forms.newAddress.number;
+    state.inputCity = document.forms.newAddress.city;
+    state.btnSave = document.forms.newAddress.btnSave;
+    state.btnClear = document.forms.newAddress.btnClear;
+
+    state.errorCep = document.querySelector('[data-error="cep"]');
+    state.errorCep = document.querySelector('[data-error="number"]');
+
+
+    state.inputNumber.addEventListener('change', handleInputNumberChange);
+    state.btnClear.addEventListener('click', handleBtnClearClick);
+    state.btnSave.addEventListener('click', handleBtnSaveClick);
+
+    state.inputCep.addEventListener('change', handleInputCepChange);
+    state.inputNumber.addEventListener('keyup', handleInputNumberKeyup);
+    
+}
+
+function handleInputNumberKeyup(event){
+    state.address.number = event.target.value;
+}
+
+async function handleInputCepChange(event){
+    try {
+        const cep = event.target.value;
+        const address = await addressService.findByCep(cep);
+        state.inputCity.value = address.city;                                                                           
+        state.inputStreet.value = address.street;
+        state.address = address;
+        setFormError("cep", "");
+        state.inputNumber.focus();
+    }catch(e){
+        state.inputStreet.value = "";
+        state.inputCity.value = "";
+        setFormError("cep", "Informe um cep valido");
+    }
+    
+}
+
+function handleInputNumberChange(event){
+    if(event.target.value == ''){
+        setFormError("number", "Campo Requerido")
+    }else{
+        setFormError("number", "")
+    }
+}                                                                                                               
+
+function handleBtnClearClick(event){
+    event.preventDefault();
+    clearForm();
+}
+
+async function handleBtnSaveClick(event){
+    event.preventDefault();
+    const errors = addressService.getErrors(state.address);
+    const keys = Object.keys(errors);
+    if(keys.length > 0){
+        for(let i = 0; i < keys.length; i++) {
+            setFormError(keys[i], errors[keys[i]]);
+        }
+    }else{
+        listController.addCard(state.address);
+        clearForm();
+    }
+    
+}
+
+function clearForm(){
+    state.inputCep.value = "";
+    state.inputCity.value = "";
+    state.inputNumber.value = "";
+    state.inputStreet.value = "";
+
+    setFormError("cep", "");
+    setFormError("number", "");
+
+    state.address = new Address();
+
+    state.inputCep.focus();
+}
+
+function setFormError(key, value) {
+    const element = document.querySelector(`[data-error="${key}"]`)
+    element.innerHTML = value;
+}
